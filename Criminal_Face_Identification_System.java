59bdf6dc7f2a46a0c681ffff5daf6756710f2279
AbbuVeena <167417120+AbbuVeena@users.noreply.github.com>
2024-04-18 15:23:23 +0530

    Add files via upload

---
+++ Criminal_Face_Identification_System.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package criminal_face_identification_system;
+
+/**
+ *
+ * @author jp
+ */
+public class Criminal_Face_Identification_System {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
